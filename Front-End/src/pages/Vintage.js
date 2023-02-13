85abc07c5a6dc3d9db2be26038635c8f7cbf898f
ejackson1228 <ejackson1228@gmail.com>
2023-02-13 16:32:27 -0500

    added cards for items on vintage page

--- Front-End/src/pages/Vintage.js
+++ Front-End/src/pages/Vintage.js
@@ -1,10 +1,46 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import testJacket from '../Assets/Images/testBomberJacket.jpg';
 
 function Vintage() {
     return (
         <div>
-            <div>
-                Vintage page
+            <div className="card-container">
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={testJacket} alt="test jacket" className="item-image"/>
+                    <Card.Body>
+                        <Card.Title>Vintage AE 80's Bomber</Card.Title>
+                        <Card.Text>$46.99</Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={testJacket} alt="test jacket" className="item-image"/>
+                    <Card.Body>
+                        <Card.Title>Vintage AE 80's Bomber</Card.Title>
+                        <Card.Text>$46.99</Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={testJacket} alt="test jacket" className="item-image"/>
+                    <Card.Body>
+                        <Card.Title>Vintage AE 80's Bomber</Card.Title>
+                        <Card.Text>$46.99</Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={testJacket} alt="test jacket" className="item-image"/>
+                    <Card.Body>
+                        <Card.Title>Vintage AE 80's Bomber</Card.Title>
+                        <Card.Text>$46.99</Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={testJacket} alt="test jacket" className="item-image"/>
+                    <Card.Body>
+                        <Card.Title>Vintage AE 80's Bomber</Card.Title>
+                        <Card.Text>$46.99</Card.Text>
+                    </Card.Body>
+                </Card>
             </div>
         </div>
     )
